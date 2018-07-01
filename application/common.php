dd2c44b0447fe4952ad08afbcf8f8d1fb4acefc0
Starexz <ksdc0338640@163.com>
2018-07-01 20:07:43 +0800

    11

--- application/common.php
+++ application/common.php
@@ -10,14 +10,3 @@
 // +----------------------------------------------------------------------
 
 // 应用公共文件
-
-
-function api_success($message = '', $data = []) {
-	return ['flag' => 'success', 'message' => $message, 'data' => $data];
-}
-
-
-
-function api_error($message = '', $code = '', $data = []) {
-	return ['flag' => 'error', 'message' => $message, 'err_code' => $code, 'data' => $data];
-}
